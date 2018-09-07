3d85caf4a856a18ee3ae4487c89c99cb2bceb6db
sanjurm16 <sanjurm.16@gmail.com>
2018-09-07 22:02:13 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+    name="spark-prometheus",
+    version="0.1",
+    scripts=['src/main.py'],
+    install_requires=['pyspark'],
+    packages=['src'],
+    author="Sanjay Mishra",
+    author_email="admin@devrats.com",
+    description="This is to demonstrate monitoring using prometheus",
+    keywords="pyspark prometheus monitoring",
+    url="http://devrats.com",
+    project_urls={
+        "Source Code": "https://github.com/sanjurm16/spark-prometheus"
+    }
+)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import argparse
 
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
 
-def main(spark:SparkSession, args):
-    spark_df = spark.read.csv(path=args.csv_file_location, inferSchema=True, header=True)
-    spark_df.write.parquet(path=args.parquet_file_op_path)
+def main(spark: SparkSession, input_path: str, output_path: str):
+    spark_df = spark.read.csv(path=input_path, inferSchema=True, header=True)
+    spark_df.write.parquet(path=output_path)
 
 
 if __name__ == '__main__':
@@ -14,4 +15,4 @@ if __name__ == '__main__':
     parser.add_argument("csv_file_ip_path", help="location of some csv files")
     parser.add_argument("parquet_file_op_path", help="location of output path")
     args = parser.parse_args()
-    main(spark_session, args)
+    main(spark_session, args.csv_file_ip_path, args.parquet_file_op_path)
